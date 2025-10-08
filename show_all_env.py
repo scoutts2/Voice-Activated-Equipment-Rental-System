45c58b7934306d664936bd5d4c4463d1a9940225
Samuel Coutts <srcoutts@mit.edu>
2025-10-07 19:00:11 -0700

    Remove Debug fs

--- show_all_env.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-Show all environment variables
-"""
-import os
-
-print("=== ALL Environment Variables ===")
-for key, value in sorted(os.environ.items()):
-    print(f"{key}: {value}")
